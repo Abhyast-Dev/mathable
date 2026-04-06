db52e7a03e5787107f1565b8a1990281fdb5781f
Abhyast-Dev <65902037+Abhyast-Dev@users.noreply.github.com>
2026-04-06 19:41:36 +0530

    Add files via upload

---
+++ worksheet.js
@@ -0,0 +1,8 @@
+// worksheet.js
+const ABLE_PDF = {
+    download: function() {
+        // Trigger the browser's native print engine
+        // This captures MathJax symbols perfectly
+        window.print();
+    }
+};
