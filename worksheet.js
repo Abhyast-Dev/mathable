10292df0d059cd4891c16187b949a9b07092df45
Abhyast-Dev <65902037+Abhyast-Dev@users.noreply.github.com>
2026-06-09 23:33:32 +0530

    Delete worksheet.js

--- worksheet.js
+++
@@ -1,8 +0,0 @@
-// worksheet.js
-const ABLE_PDF = {
-    download: function() {
-        // Trigger the browser's native print engine
-        // This captures MathJax symbols perfectly
-        window.print();
-    }
-};
